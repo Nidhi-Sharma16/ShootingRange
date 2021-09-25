2be23ee2ccf1e208d289ce73b28103279f22c4e9
Nidhi-Sharma16 <79085182+Nidhi-Sharma16@users.noreply.github.com>
2021-09-25 15:04:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+var life =3;
+var score=0;
+var gameState=1
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
+}
+function setup() {
+  createCanvas(800, 700);
+
+  life =createElement("h1");
+  heading = createElement("h1");
+  scoreboard = createElement("h1");
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+
+ 
+
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  
+}
+
+function draw() {
+  background("#BDA297");
+
+  scoreboard.html("Score: "+score);
+  scoreboard.style('color:red');
+  scoreboard.position(width-200,20);
+
+  life.html("Life:");
+  life.style('color:red');
+  life.position(200,20)
+
+  //display Score and number of lifes
+  
+  if(gameState===1){
+    gun.y=mouseY 
+    
+    if(frameCount % 80===0){
+      drawBlueBubble();
+    }
+   
+    if(frameCount % 100===0){
+      drawRedBubble();
+    }
+
+    
+    drawSprites();
+  }
+  
+}
+
+function ShootBullet(){
+
+  bullet = createSprite(150,gun.y-20,20,10);
+    bullet.addImage(bulletImg);
+  
+}  
+
+function drawBlueBubble(){
+
+bluebubble = createSprite(800,random(20,780),40,40);
+bluebubble.addImage(blueBubbleImg);
+bluebubble.scale = 0.1;
+bluebubble.velocityX =-8;
+bluebubble.lifetime = 400;
+blueBubbleGroup.add(bluebubble);
+}
+
+function drawRedBubble(){
+
+  redbubble = createSprite(800,random(20,780),50,50);
+  redbubble.addImage(redBubbleImg);
+  redbubble.scale = 0.1;
+  redbubble.velocityX =-9;
+  redbubble.lifetime = 400;
+  redBubbleGroup.add(redbubble);
+  }
+  
+  function handleBubbleCollision(bubbleGroup){
+    if(life > 0){
+      score = score+1;
+    }
+  }
+
+
+
